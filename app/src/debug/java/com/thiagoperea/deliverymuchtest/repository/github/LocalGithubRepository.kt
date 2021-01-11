a675bb4c981f489a62bd6a7ef048c8a84aef60b1
Thiago <thiago.perea1@gmail.com>
2021-01-11 08:41:54 -0300

    [bugfix] removing random errors

--- app/src/debug/java/com/thiagoperea/deliverymuchtest/repository/github/LocalGithubRepository.kt
+++ app/src/debug/java/com/thiagoperea/deliverymuchtest/repository/github/LocalGithubRepository.kt
@@ -8,39 +8,35 @@ import kotlin.random.Random
 class LocalGithubRepository : GithubRepository {
 
     override suspend fun doSearch(searchQuery: String): Response<List<Repository>> {
-        return if (Random.nextBoolean()) {
-            Response(false, null, "Some mock exception!")
-        } else {
-            Response(
-                true,
-                listOf(
-                    Repository(
-                        "Mock repository 1",
-                        RepositoryAuthor("Author 1", ""),
-                        "Description 1"
-                    ),
-                    Repository(
-                        "Mock repository 2",
-                        RepositoryAuthor("Author 2", ""),
-                        "Description 2"
-                    ),
-                    Repository(
-                        "Mock repository 3",
-                        RepositoryAuthor("Author 3", ""),
-                        "Description 3"
-                    ),
-                    Repository(
-                        "Mock repository 4",
-                        RepositoryAuthor("Author 4", ""),
-                        "Description 4"
-                    ),
-                    Repository(
-                        "Mock repository 5",
-                        RepositoryAuthor("Author 5", ""),
-                        "Description 5"
-                    ),
-                )
+        return Response(
+            true,
+            listOf(
+                Repository(
+                    "Mock repository 1",
+                    RepositoryAuthor("Author 1", ""),
+                    "Description 1"
+                ),
+                Repository(
+                    "Mock repository 2",
+                    RepositoryAuthor("Author 2", ""),
+                    "Description 2"
+                ),
+                Repository(
+                    "Mock repository 3",
+                    RepositoryAuthor("Author 3", ""),
+                    "Description 3"
+                ),
+                Repository(
+                    "Mock repository 4",
+                    RepositoryAuthor("Author 4", ""),
+                    "Description 4"
+                ),
+                Repository(
+                    "Mock repository 5",
+                    RepositoryAuthor("Author 5", ""),
+                    "Description 5"
+                ),
             )
-        }
+        )
     }
 }
